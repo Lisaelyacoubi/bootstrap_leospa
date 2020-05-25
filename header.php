6917b68ac43fb2152b15797e0ecfc8b090139e6e
lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-05-25 16:58:27 +0200

    police

--- header.php
+++ header.php
@@ -12,8 +12,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
     <!-- POLICE -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Rufina:400,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet"> 
 
     <script src="https://kit.fontawesome.com/5bf2af5d34.js" crossorigin="anonymous"></script>
 
