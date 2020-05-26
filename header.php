f948a4b48ef5448e23c83122c4399a0bce978ade
lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-05-26 16:29:04 +0200

    add comments in header

--- header.php
+++ header.php
@@ -11,9 +11,10 @@
     <!-- BOOTSTRAP-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
-    <!-- POLICE -->
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet"> 
+    <!-- GOOGLE FONT -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
 
+    <!-- FONTAWESOME -->
     <script src="https://kit.fontawesome.com/5bf2af5d34.js" crossorigin="anonymous"></script>
 
 
